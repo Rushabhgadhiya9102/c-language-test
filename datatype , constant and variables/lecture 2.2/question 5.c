cdf88f0ef71b01455d0062befe8ab3d6d8eb0d69
Rushabhgadhiya9102 <158015850+Rushabhgadhiya9102@users.noreply.github.com>
2024-02-08 11:24:31 +0530

    Update question 5.c

--- datatype , constant and variables/lecture 2.2/question 5.c
+++ datatype , constant and variables/lecture 2.2/question 5.c
@@ -1,14 +1,13 @@
 //perimeter of circle
 
 #include <stdio.h>
-#define PI 3.14
 int main(){
 
   int radius, perimeter;
   printf("enter the value of radius : ");
   scanf("%d",&radius);
 
-   perimeter = 2*PI*radius;
+   perimeter = 2*3.14*radius;
 
   printf("the perimeter of circle is : %d",perimeter);
 
