e3502a6cde28273adae185cfd015fd0f29a1d497
Rushabhgadhiya9102 <158015850+Rushabhgadhiya9102@users.noreply.github.com>
2024-02-26 12:48:46 +0530

    question 2.c

--- greenhorns/lecture 6.1/question 2.c
+++ greenhorns/lecture 6.1/question 2.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 
-int main(void) { 
-  
-int BS;
-int HRA;
-int TA;
-int DA;
+int main(){
 
-  printf("enter the number of BS :");
-  scanf("%d",&BS);
-  
-  printf("\nenter the number of HRA :");
-  scanf("%d",&HRA);
-  
-  printf("\nenter the number of TA :");
-  scanf("%d",&TA);
-  
-  printf("\nenter the number of DA :");
-  scanf("%d",&DA);
-  
-  printf("\n your salary is : %d",BS+HRA+DA+TA);
+  float basic_salary, hra_percentage, ta_percentage, da_percentage;
+  float gross_salary, hra, ta, da;
+
+  printf("Enter the basic salary: ");
+  scanf("%f", &basic_salary);
+
+  printf("Enter the HRA percentage: ");
+  scanf("%f", &hra_percentage);
+
+  printf("Enter the TA percentage: ");
+  scanf("%f", &ta_percentage);
+
+  printf("Enter the DA percentage: ");
+  scanf("%f", &da_percentage);
+
+  hra = basic_salary * (hra_percentage / 100);
+  ta = basic_salary * (ta_percentage / 100);
+  da = basic_salary * (da_percentage / 100);
+
+  gross_salary = basic_salary + hra + ta + da;
+
+  printf("HRA: %.2f\n",hra);
+  printf("TA: %.2f\n",ta);
+  printf("DA: %.2f\n",da);
+  printf("Gross Salary: %.2f\n",gross_salary);
 
   return 0;
 }
