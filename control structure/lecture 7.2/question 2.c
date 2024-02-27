ae9cb57920f766a7358795a135f7f8873efcea4e
Rushabhgadhiya9102 <158015850+Rushabhgadhiya9102@users.noreply.github.com>
2024-02-27 11:11:56 +0530

    question 2.c

--- control structure/lecture 7.2/question 2.c
+++ control structure/lecture 7.2/question 2.c
@@ -1,35 +1,49 @@
-//maximum number
+#include <stdio.h>
+int main() {
 
-#include  <stdio.h>
-int main(){
+    int a, b, c, d;
 
-int a,b,c,d;
+    printf("Enter first numbers: ");
+    scanf("%d",&a);
 
-  printf ("enter the value of a :");
-  scanf ("%d",&a);
+    printf("Enter second numbers: ");
+    scanf("%d",&b);
 
-  printf ("enter the value of b :");
-  scanf ("%d",&b);
+    printf("Enter third numbers: ");
+    scanf("%d",&c);
 
-  printf ("enter the value of c :");
-  scanf("%d",&c);
-
-  printf ("enter the value of d :");
-  scanf("%d",&d);
-
-  if (a>d){
-    if(a>b){
-      printf ("%d is the biggest number",a);
-    }else{
-      printf ("%d is the biggest number",b);
-    }       
-    }else if (b>d){
-      printf ("%d is the biggest number",b);
-    }else if(b>c){
-      printf ("%d is the biggest number",c);
-    }else{
-      printf ("%d is the biggest number",d);
-    }
+    printf("Enter four numbers: ");
+    scanf("%d",&d);
   
-  return 0;
+    if (a >= b) {
+        if (a >= c) {
+            if (a >= d) {
+                printf("The maximum number is: %d\n", a);
+            } else {
+                printf("The maximum number is: %d\n", d);
+            }
+        } else {
+            if (c >= d) {
+                printf("The maximum number is: %d\n", c);
+            } else {
+                printf("The maximum number is: %d\n", d);
+            }
+        }
+    } else {
+        if (b >= c) {
+            if (b >= d) {
+                printf("The maximum number is: %d\n", b);
+            } else {
+                printf("The maximum number is: %d\n", d);
+            }
+        } else {
+            if (c >= d) {
+                printf("The maximum number is: %d\n", c);
+            } else {
+                printf("The maximum number is: %d\n", d);
+            }
+        }
+    }
+
+    return 0;
 }
