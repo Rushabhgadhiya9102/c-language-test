6ba8052040df6c8747ca8bc9f7976bd95a985687
Rushabhgadhiya9102 <158015850+Rushabhgadhiya9102@users.noreply.github.com>
2024-03-02 11:00:48 +0530

    que4.c

--- assortment/que4.c
+++ assortment/que4.c
@@ -10,11 +10,12 @@
      scanf("%d",&b);
 
      int arr[a][b];
-     printf("enter the element of the 2d array :\n");
+    
      for (int i=0;i<a;i++)
      {
          for(int j=0;j<b;j++)
          {
+             printf("enter the element of (%d,%d) : ",i,j);
              scanf("%d",&arr[i][j]);
          }
      }
