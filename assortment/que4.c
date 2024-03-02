c4efffac83035bc77369f1720b6211e7b28688c9
Rushabhgadhiya9102 <158015850+Rushabhgadhiya9102@users.noreply.github.com>
2024-03-02 10:46:00 +0530

    que4.c

--- assortment/que4.c
+++ assortment/que4.c
@@ -4,10 +4,10 @@
 
      int a, b;
      printf("enter the number of rows :");
-     scanf("%d",&b);
+     scanf("%d",&a);
 
      printf("enter the number of columns :");
-     scanf("%d",&a);
+     scanf("%d",&b);
 
      int arr[a][b];
      printf("enter the element of the 2d array :\n");
