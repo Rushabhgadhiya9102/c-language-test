8871d5b899ab8083d20518187a2d1bfe9a8a03a8
Rushabhgadhiya9102 <158015850+Rushabhgadhiya9102@users.noreply.github.com>
2024-03-20 16:17:11 +0530

    que2.c

--- supervision/que2.c
+++ supervision/que2.c
@@ -2,13 +2,12 @@
 
 struct students{
 
-    int rn;
+    int roll_no;
     char name[50];
-    int chem;
+    int chemistry;
     int maths;
     int physics;
 };
-
 
 int main(){
 
@@ -16,46 +15,48 @@ struct students students[5];
 
 float percentage(int total_marks)
 {
-    return(float)total_marks/300*100;
+    return (float)total_marks/300*100;
 }
 
-  for(int i=0 ; i<5 ; i++)
-  {
-     printf("\nEnter details of Student %d:\n", i + 1);
-        printf("Roll no : ");
-        scanf("%d",&students[i].rn);
+for(int i=0;i<5;i++)
+{
+    printf("enter the details of student (%d) \n\n",i+1);
+    printf("Roll_no : ");
+    scanf("%d",&students[i].roll_no);
 
-        printf("Name : ");
-        scanf("%s",&students[i].name);
+    printf("Name : ");
+    scanf("%s",&students[i].name);
 
-        printf("Chemistry : ");
-        scanf("%d", &students[i].chem);
+    printf("Chemistry : ");
+    scanf("%d",&students[i].chemistry);
 
-        printf("Mathematics : ");
-        scanf("%d", &students[i].maths);
+    printf("Maths : ");
+    scanf("%d",&students[i].maths);
 
-        printf("Physics : ");
-        scanf("%d", &students[i].physics);
-    }
-    printf("\nOutput:\n\n");
+    printf("Physics : ");
+    scanf("%d",&students[i].physics);
 
-    for (int i = 0; i < 5; i++) 
+}
+
+    printf("\nOutput\n\n");
+
+    for(int i=0;i<5;i++)
     {
-        printf("%s (%d)\n", students[i].name, students[i].rn);
+        printf("%s (%d) \n",students[i].name, students[i].roll_no);
 
-        printf("Chemistry = %d\n", students[i].chem);
+        printf("Chemitry : %d\n",students[i].chemistry);
 
-        printf("Mathematics = %d\n", students[i].maths);
+        printf("Maths : %d\n",students[i].maths);
 
-        printf("Physics = %d\n", students[i].physics);
+        printf("Physics : %d\n",students[i].physics);
 
-        int total_marks = students[i].chem + students[i].maths + students[i].physics;
+         int total_marks = students[i].chemistry + students[i].maths + students[i].physics;
 
-        printf("Total = %d/300\n", total_marks);
+        printf("Total = %d/300\n",total_marks);
 
-        printf("Percent = %.2f%%\n\n", percentage(total_marks));
- }
-  
+        printf("Percent = %2f%%\n\n",percentage(total_marks));
+
+    }
 
     return 0;
 }
